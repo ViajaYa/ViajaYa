35680b407f3cfaeb95ad0aeaf6586ff1d8ad597f
yanirc1981 <cerzurc@hotmail.com>
2024-08-20 18:06:16 -0300

    Se agregan redes en Navbar

--- viajaya/src/components/layout/NavBar/NavBar.jsx
+++ viajaya/src/components/layout/NavBar/NavBar.jsx
@@ -5,10 +5,14 @@ import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
-const NavBar = ({ ruta, showFullMenu = true }) => { // Añadido `showFullMenu`
+// Importa los íconos de redes sociales
+import { FaFacebookF, FaInstagram, FaTiktok, FaTelegramPlane, FaWhatsapp } from 'react-icons/fa';
+
+const NavBar = ({ ruta, showFullMenu = true }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState();
   const linkRef = useRef();
+  const [showSocialMenu, setShowSocialMenu] = useState(false);
 
   const verify = async () => {
     const data = await axios.get(`/user/verify/${localStorage.getItem("token")}`);
@@ -33,7 +37,7 @@ const NavBar = ({ ruta, showFullMenu = true }) => { // Añadido `showFullMenu`
             <ScrollLink to="home" smooth={true} duration={500}>
               <li className={style.li}>Inicio</li>
             </ScrollLink>
-            <RouterLink to="/about" style={{ textDecoration: "none" }}>
+            <RouterLink to="/about" className={style.noLink}>
               <li className={style.li}>Sobre Nosotros</li>
             </RouterLink>
             <ScrollLink to="proyectos" smooth={true} duration={500}>
@@ -50,18 +54,46 @@ const NavBar = ({ ruta, showFullMenu = true }) => { // Añadido `showFullMenu`
             </ScrollLink>
           </>
         ) : (
-          <RouterLink to="/" style={{ textDecoration: "none" }}>
-          <li className={style.li}>Inicio</li>
-        </RouterLink>
+          <RouterLink to="/" className={style.noLink}>
+            <li className={style.li}>Inicio</li>
+          </RouterLink>
         )}
         {user ? (
           <li className={style.libutton} onClick={() => navigate(`/profile`)}>{user.name}</li>
         ) : (
           <li className={style.libutton} onClick={() => navigate("/login")}>Ingresar</li>
         )}
+        {/* Sección de redes sociales */}
+        <li className={style.socialMenu}>
+          <button onClick={() => setShowSocialMenu(!showSocialMenu)} className={style.socialButton}>
+            Redes
+          </button>
+          {showSocialMenu && (
+            <div className={style.socialIcons}>
+              <a href="https://wa.link/28unmk"target="_blank" rel="noopener noreferrer" className={style.icon}>
+                <FaWhatsapp />
+              </a>
+              <a href="https://www.facebook.com/oficialviajaya/" target="_blank" rel="noopener noreferrer" className={style.icon}>
+                <FaFacebookF />
+              </a>
+              <a href="https://www.instagram.com/viajaya_pagina_oficial/"  target="_blank" rel="noopener noreferrer" className={style.icon}>
+                <FaInstagram />
+              </a>
+              <a href="https://www.tiktok.com/@agenciadeviajesviajaya" target="_blank" rel="noopener noreferrer" className={style.icon}>
+                <FaTiktok />
+              </a>
+              <a href="https://www.t.me/+jVPYyJBifRJiMjdh" target="_blank" rel="noopener noreferrer" className={style.icon}>
+                <FaTelegramPlane />
+              </a>
+            </div>
+          )}
+        </li>
       </ul>
     </nav>
   );
 };
 
 export default NavBar;
+
+
+
