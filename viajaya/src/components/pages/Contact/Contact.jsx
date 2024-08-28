857896a3b8b4f982c2807357321e7d4bf7f96f9f
yanirc1981 <cerzurc@hotmail.com>
2024-08-28 09:53:13 -0300

    Se cambia carousel.

--- viajaya/src/components/pages/Contact/Contact.jsx
+++ viajaya/src/components/pages/Contact/Contact.jsx
@@ -75,10 +75,10 @@ const Contact = () => {
   <Element name="contactanos">
     <Toaster></Toaster>
     <motion.div ref={ref} animate={animation} className={style.contact} id="contactanos">
-      <h2 className={style.titleSection}>Trabaja con nosotros</h2>
+      
       <div className={style.contactContainer}>
         <div className={style.infoContacto}>
-          <h2 className={style.subtitleSection}>Informacion de contacto</h2>
+       
           <div className={style.correos}>
         <div>
           <FiMail className={style.icon}/>
