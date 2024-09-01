64e370e772036f39a4368157c2bd89adc72f09d4
mlobeto <mercedeslobeto@gmail.com>
2024-09-01 10:13:07 -0300

    sin proo

--- viajaya/src/components/pages/ProfileAdmin/Profile.jsx
+++ viajaya/src/components/pages/ProfileAdmin/Profile.jsx
@@ -9,11 +9,7 @@ import { toast, Toaster } from "react-hot-toast";
 import dayjs from "dayjs";
 import "dayjs/locale/es";
 
-
-
-
-
-import proo from '../../../assets/newImg/viajaYaImg/proo.png';
+//import proo from '../../../assets/newImg/viajaYaImg/proo.png';
 
 import NavBar from '../../layout/NavBar/NavBar';
 
@@ -231,7 +227,7 @@ const Profile = () => {
                 </div>
       
                 {/* Imagen en la mitad izquierda */}
-                <div className="w-1/2 lg:block flex items-center justify-center mt-32 ">
+                {/* <div className="w-1/2 lg:block flex items-center justify-center mt-32 ">
   <Link to="/productos">
     <img
 
@@ -241,7 +237,7 @@ const Profile = () => {
       className="w-1/2 h-auto max-w-md ml-32 cursor-pointer border-4 border-ColorAzul"
     />
   </Link>
-</div>
+</div> */}
               </div>
             </div>
           )}
