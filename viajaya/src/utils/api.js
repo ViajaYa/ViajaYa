ef00068ff7680d66d5c9e14090cda6ca1f463d87
yanirc1981 <cerzurc@hotmail.com>
2025-08-09 10:59:13 -0300

    probando 2

--- viajaya/src/utils/api.js
+++ viajaya/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '../redux/store/store';
 import { logout } from '../redux/slices/authSlice';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'; //probando 2
 
 // Crear instancia de axios
 const api = axios.create({
