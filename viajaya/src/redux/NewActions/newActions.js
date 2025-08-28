55dfc8dfa65c04fd3b8668f15bd6e7fef60d1a63
mlobeto <mercedeslobeto@gmail.com>
2025-08-28 17:07:43 -0300

    para render

--- viajaya/src/redux/NewActions/newActions.js
+++ viajaya/src/redux/NewActions/newActions.js
@@ -51,7 +51,7 @@ import {
 } from "./NewActions-Types";
 
 
-const BASE_URL = 'https://viajaya-production.up.railway.app';
+const BASE_URL = 'https://viajaya-mve8.onrender.com';
 //const BASE_URL = 'http://localhost:3001'
 
 
--- viajaya/src/redux/actions/actions.js
+++ viajaya/src/redux/actions/actions.js
@@ -29,7 +29,7 @@ USER_REGISTER_SUCCESS,
 USER_REGISTER_FAIL,
 } from './actions-types'
 
-const BASE_URL = 'https://viajaya-production.up.railway.app'
+const BASE_URL = 'https://viajaya-mve8.onrender.com'
 //const BASE_URL = 'http://localhost:3001'
 
 export const setUser = (user) => {
