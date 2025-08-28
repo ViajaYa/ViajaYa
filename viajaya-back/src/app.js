61951aee4f59f0d7428614b8fe4fd6aa541881c3
mlobeto <mercedeslobeto@gmail.com>
2025-08-28 12:04:10 -0300

    test

--- viajaya-back/src/app.js
+++ viajaya-back/src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 const routes = require("./routes/index.js")
+const { addSmtpTestEndpoint } = require("./utils/emailService.js");
 
 const allowedOrigins = [
     'http://localhost:5173', // Permitir localhost para desarrollo
@@ -14,6 +15,8 @@ const app = express()
 app.use(express.json())
 app.use(cors());
 app.use(morgan("dev"))
+// Endpoint temporal para probar conectividad SMTP
+addSmtpTestEndpoint(app);
 app.use("/", routes)
 // app.use((req, res, next) => {
 //     res.setHeader(
--- viajaya-back/src/utils/emailService.js
+++ viajaya-back/src/utils/emailService.js
@@ -51,4 +51,24 @@ const sendEmail = async (mailOptions) => {
     throw error; 
   }
 };
-module.exports = { sendEmail };
+// Endpoint temporal para probar conectividad SMTP desde Railway
+const net = require('net');
+const addSmtpTestEndpoint = (app) => {
+  app.get('/test-smtp', (req, res) => {
+    const socket = net.createConnection(465, 'smtp.gmail.com');
+    socket.setTimeout(5000);
+    socket.on('connect', () => {
+      res.send('Conexión SMTP exitosa');
+      socket.end();
+    });
+    socket.on('timeout', () => {
+      res.status(500).send('Timeout al conectar al SMTP');
+      socket.destroy();
+    });
+    socket.on('error', (err) => {
+      res.status(500).send('Error al conectar al SMTP: ' + err.message);
+    });
+  });
+};
+
+module.exports = { sendEmail, addSmtpTestEndpoint };
